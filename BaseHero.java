6eb1f7cb7b89908c9398f21e2fa5e9125583a7d1
DanteMilten <milten47@yahoo.com>
2023-07-11 20:38:46 +0300

    Seminar_2

--- BaseHero.java
+++ BaseHero.java
@@ -1,5 +1,4 @@
-public class BaseHero {
-
+public abstract class BaseHero implements BaseHeroInterface {
     protected String name;
 
     protected int strength;
@@ -10,7 +9,7 @@ public class BaseHero {
     protected int health;
     protected int healthMax;
 
-
+    // Полный конструктор
     public BaseHero(String name,
                     int strength, int dexterity, int intelligence, int endurance,
                     int health, int healthMax) {
@@ -25,41 +24,51 @@ public class BaseHero {
         this.healthMax    = healthMax;
     }
 
+    // Краткий конструктор, через this(...)
     public BaseHero( String name, int strength, int health ) {
-        this.name         = name;
-        this.strength     = strength;
-        this.health       = health;
-        this.healthMax    = health;
+        this(
+                name,
+                strength,
+                0,
+                0,
+                0,
+                health,
+                health );
     }
 
-    public String getName()      { return name; }
+    // Пустой конструктор, через this(...)
+    public BaseHero() {
+        this( "unknown", 1, 0, 0, 0, 5, 5 );
+    }
 
+
+    // Геттеры, для изменения значений приватных полей
+    @Override
+    public String getName()      { return name; }
     public int getStrength()     { return strength; }
     public int getDexterity()    { return dexterity; }
     public int getIntelligence() { return intelligence; }
     public int getEndurance()    { return endurance; }
-
     public int getHealth()       { return health; }
     public int getHealthMax()    { return healthMax; }
 
-
+    // Сеттеры, для получения значений приватных полей
     public void setName(         String name )      { this.name         = name; }
-
     public void setStrength(     int strength )     { this.strength     = strength; }
     public void setDexterity(    int dexterity )    { this.dexterity    = dexterity; }
     public void setIntelligence( int intelligence ) { this.intelligence = intelligence; }
     public void setEndurance(    int endurance )    { this.endurance    = endurance; }
-
     public void setHealth(       int health )       { this.health       = health; }
     public void setHealthMax(    int health )       { this.healthMax    = health; }
 
-    // Умер
+
+    // Действие - Смерть
     public void die() {
         setHealth( 0 );
         setHealthMax( 0 );
     }
 
-    // Ранен
+    // Действие - Ранение
     public void takeDamage( int damage ) {
         if ( this.health > damage ) {
             this.health -= damage;
@@ -70,7 +79,7 @@ public class BaseHero {
         }
     }
 
-    // Атакует
+    // Действие - Атака
     public void attack( BaseHero target ) {
         int damage = this.strength * 1;
         target.takeDamage( damage );
@@ -78,12 +87,14 @@ public class BaseHero {
 
     }
 
-    // Лечится, не больше максимума
+    // Действие - Лечение (не больше максимума)
     public void rest( int hp ) {
         this.health = this.health + hp > this.healthMax ? this.healthMax : hp + this.health;
         System.out.println( this.name + " rests and recovers " + hp + " health points." );
     }
 
+
+    // Вывод в строковом виде (краткий, т.к. не все...)
     public String toString() {
         return String.format("-- %s -- %s[%d/%d], Сила: %d --",
                 this.getClass().getName(), this.name, this.health, this.healthMax, this.strength);
